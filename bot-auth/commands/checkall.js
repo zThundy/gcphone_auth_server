ac9e0921c8627e710d37222a39113e4498e2e312
zThundy <antonioantonelli7@gmail.com>
2021-07-30 14:06:34 +0200

    finished checkall command

--- bot-auth/commands/checkall.js
+++ bot-auth/commands/checkall.js
@@ -11,10 +11,10 @@ exports.onMessage = (message, channel, guild, extradata) => {
     // const Members = guild.members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => [member.user.tag, member.user.id]);
     const Members = guild.roles.cache.get(config.guild_info.role_id).members.map(member => [member.user.tag, member.user.id]);
     console.log(Members)
-    CheckUser(extradata, Members, 0)
+    CheckUser(extradata, Members, 0, channel)
 }
 
-const CheckUser = (extradata, tb, index) => {
+const CheckUser = (extradata, tb, index, channel) => {
     console.log("received arguments on checkuser function", tb[index], index);
     if (tb[index]) {
         extradata.conn.query("SELECT * FROM licenses WHERE discord_id = ?", [tb[index][1]], (err, r, _) => {
@@ -22,15 +22,37 @@ const CheckUser = (extradata, tb, index) => {
             console.log("before the undefined check", JSON.stringify(r[0]), tb[index][0], tb[index][1])
             if (r[0] === undefined) {
                 console.log("after the undefined check", JSON.stringify(r[0]), tb[index][0], tb[index][1])
-                extradata.conn.query("INSERT INTO licenses(discord_id, license) VALUES(?, ?)", [tb[index][1], utils.createLicense(40)], (err, __, _) => {
+                extradata.conn.query("INSERT INTO licenses(discord_id, license) VALUES(?, ?)", [tb[index][1], utils.createLicense(40)], (err, i, _) => {
+                    console.log(JSON.stringify(i));
                     if (err) throw err;
-                    if (tb[index + 1]) {
-                        CheckUser(extradata, tb, index + 1);
-                    }
+                    extradata.conn.query("SELECT * FROM ips WHERE account_id = ?", [i.insertId], (err, ips, _) => {
+                        if (err) throw err;
+                        if (ips[0] === undefined || ips[1] === undefined) {
+                            extradata.conn.query("INSERT INTO ips(account_id) VALUES(?)", [i.insertId], (err, __, _) => {
+                                if (err) throw err;
+                                extradata.conn.query("INSERT INTO ips(account_id) VALUES(?)", [i.insertId], (err, __, _) => {
+                                    if (err) throw err;
+                                    if (tb[index + 1]) {
+                                        CheckUser(extradata, tb, index + 1, channel);
+                                    } else {
+                                        utils.yesEmbed("Inserting of users and ips done 😎", channel)
+                                    }
+                                });
+                            });
+                        } else { 
+                            if (tb[index + 1]) {
+                                CheckUser(extradata, tb, index + 1, channel);
+                            } else {
+                                utils.yesEmbed("Inserting of users and ips done 😎", channel)
+                            }
+                        }
+                    });
                 })
             } else {
                 if (tb[index + 1]) {
-                    CheckUser(extradata, tb, index + 1);
+                    CheckUser(extradata, tb, index + 1, channel);
+                } else {
+                    utils.yesEmbed("Inserting of users and ips done 😎", channel)
                 }
             }
         })
